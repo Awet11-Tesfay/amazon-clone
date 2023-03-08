4cab83cb289e367baa737387b1e282d1f898e302
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-08 20:50:48 +0300

    building_react_app

---
+++ public/build/precache-manifest.a17a0ebdf893467ca2a280e109751bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58cc435715f9329757d94fee151cd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f16e02d789d9b00d1e",
+    "url": "/static/css/main.be2f8d45.chunk.css"
+  },
+  {
+    "revision": "d160828ba32a0e7f3ed1",
+    "url": "/static/js/2.7f42808d.chunk.js"
+  },
+  {
+    "revision": "df096a53381dc48c2c08c9530047e7ba",
+    "url": "/static/js/2.7f42808d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f16e02d789d9b00d1e",
+    "url": "/static/js/main.b69be3ed.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
